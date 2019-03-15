36156f129f015ff43513be826e1c4bb56b03085c
Abel Acosta <abelacosta1596@gmail.com>
2019-03-15 16:25:02 -0700

    Added functions for login

---
+++ js/dashboard.js
@@ -0,0 +1,25 @@
+function onLoad() {
+      gapi.load('auth2', function() {
+        gapi.auth2.init().then(function(){
+          auth2 = gapi.auth2.getAuthInstance();
+          loadProfileContent(auth2);
+        });
+      });
+}
+
+function loadProfileContent(auth2){
+  if (auth2.isSignedIn.get()) {
+    let profile = auth2.currentUser.get().getBasicProfile();
+    console.log('ID: ' + profile.getId());
+    console.log('Full Name: ' + profile.getName());
+    console.log('Given Name: ' + profile.getGivenName());
+    console.log('Family Name: ' + profile.getFamilyName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+    let profileImg = document.getElementById("profile");
+    profileImg.src = profile.getImageUrl();
+
+  }else{
+    window.location.replace("http://localhost:3000/login.html");
+  }
+}
