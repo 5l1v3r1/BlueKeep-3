dfa112daa9837969d272b2c1f7a61f088fbfc338
Abel Acosta <abelacosta1596@gmail.com>
2019-03-22 18:01:33 -0700

    Moved javascript files to appropriate file

--- js/googlesignin.js
+++ js/googlesignin.js
@@ -15,3 +15,37 @@
         'onfailure': onFailure
       });
     }
+
+    function onLoad() {
+          gapi.load('auth2', function() {
+            gapi.auth2.init().then(function(){
+              auth2 = gapi.auth2.getAuthInstance();
+              loadProfileContent(auth2);
+            });
+          });
+    }
+
+    function loadProfileContent(auth2){
+      if (auth2.isSignedIn.get()) {
+        let profile = auth2.currentUser.get().getBasicProfile();
+        console.log('ID: ' + profile.getId());
+        console.log('Full Name: ' + profile.getName());
+        console.log('Given Name: ' + profile.getGivenName());
+        console.log('Family Name: ' + profile.getFamilyName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail());
+        let profileImg = document.getElementById("profile-pic");
+        profileImg.src = profile.getImageUrl();
+
+      }else{
+        window.location.replace("http://localhost:3000/login.html");
+      }
+    }
+
+    function signOut() {
+        let auth2 = gapi.auth2.getAuthInstance();
+        auth2.signOut().then(function () {
+          console.log('User signed out.');
+        });
+        window.location.replace("/");
+    }
