956d9314ac6e5b6c44273e425db519819fad51c0
Abel Acosta <abelacosta1596@gmail.com>
2019-04-05 17:47:59 -0700

    Added functions to Authentication class

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(public afAuth: AngularFireAuth) {}
+
+  isLoggedIn(){
+    let user = this.afAuth.auth.currentUser;
+    console.log(user);
+    if(user){
+      return true;
+    }
+    return false;
+  }
+
+  login(email, pass){
+    this.afAuth.auth.signInWithEmailAndPassword(email, pass).catch((error) =>{
+      console.log(error.message);
+    });
+  }
+
+  signOut(){
+    this.afAuth.auth.signOut().then(()=>{
+      // TODO: Sign out successful.
+      console.log("Sign out successful");
+    }).catch(()=>{
+      // TODO: Handle sign out error.
+    });
+  }
 }
