bc65dd4dd133dfafc6fc43d87bf19e0872663c98
Abel Acosta <abelacosta1596@gmail.com>
2019-04-05 17:46:24 -0700

    Added route to signup page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import {LoginComponent} from './login/login.component';
+import {SignupComponent} from './signup/signup.component';
 import {HomeComponent} from './home/home.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
 
 const routes: Routes = [
   {path: "", component: HomeComponent},
   {path: "login", component: LoginComponent},
+  {path: "signup", component: SignupComponent},
   {path: "dashboard", component: DashboardComponent}
 ];
 
